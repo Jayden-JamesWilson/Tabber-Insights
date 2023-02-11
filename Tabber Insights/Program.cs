5b78b4393f9dd7fcc75f48dd791bfa21fc724fc8
J-Tech Designs <JTechDesigns@outlook.com>
2023-02-11 23:26:44 +0000

    Version 0.0.0.13 Beta

--- Tabber Insights/Program.cs
+++ Tabber Insights/Program.cs
@@ -8,7 +8,8 @@ namespace Tabber_Insights
         [STAThread]
         static void Main()
         {
-            
+            Properties.Settings.Default.Upgrade();
+            Properties.Settings.Default.Save();
 
             //SetProcessDPIAware();
             // To customize application configuration such as set high DPI settings or default font,
