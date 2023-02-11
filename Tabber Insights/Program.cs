033ace28eb12fd319da3af2d7eabbdd3c104bf82
J-Tech Designs <JTechDesigns@outlook.com>
2023-02-11 23:41:53 +0000

    Version 0.0.0.16 Beta

--- Tabber Insights/Program.cs
+++ Tabber Insights/Program.cs
@@ -7,23 +7,7 @@ namespace Tabber_Insights
         /// </summary>
         [STAThread]
         static void Main()
-        {
-            string localAppData =
-            Environment.GetFolderPath(
-            Environment.SpecialFolder.LocalApplicationData);
-            string userFilePath
-              = Path.Combine(localAppData, "Tabber");
-
-            if (!Directory.Exists(userFilePath))
-                Directory.CreateDirectory(userFilePath);
-
-            //if it's not already there, 
-            //copy the file from the deployment location to the folder
-            string sourceFilePath = Path.Combine(
-              System.Windows.Forms.Application.StartupPath, "TabberInsightsData.txt");
-            string destFilePath = Path.Combine(userFilePath, "TabberInsightsData.txt");
-            if (!File.Exists(destFilePath))
-                File.Copy(sourceFilePath, destFilePath);
+        { 
 
             //SetProcessDPIAware();
             // To customize application configuration such as set high DPI settings or default font,
