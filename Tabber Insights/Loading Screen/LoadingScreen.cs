f22c9c3867286e921e7660bf1e83985764be675e
J-Tech Designs <JTechDesigns@outlook.com>
2023-02-14 03:12:02 +0000

    Version 1.0.0.4

--- Tabber Insights/Loading Screen/LoadingScreen.cs
+++ Tabber Insights/Loading Screen/LoadingScreen.cs
@@ -19,26 +19,13 @@ namespace Tabber_Insights.Loading_Screen
 
         private void LoadingScreen_Load(object sender, EventArgs e)
         {
-            BackgroundWorker worker = new BackgroundWorker();
-            worker.DoWork += Worker_DoWork;
-            worker.RunWorkerCompleted += Worker_RunWorkerCompleted;
-            worker.RunWorkerAsync();
-        }
-
-        private void Worker_DoWork(object? sender, DoWorkEventArgs e)
-        {
-            //Do long tasks here
             System.Threading.Thread.Sleep(1000);
-        }
 
-        private void Worker_RunWorkerCompleted(object? sender, RunWorkerCompletedEventArgs e)
-        {
             TabberInsights TabberInsights = new TabberInsights();
             TabberInsights.Show();
 
             this.Text = "Loaded";
             this.Hide();
         }
-
     }
 }
